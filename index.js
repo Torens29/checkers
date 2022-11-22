51118b2a0def917af9b6112a20e2254cd7a86e6d
Torens29 <44534255+Torens29@users.noreply.github.com>
2022-11-22 14:37:31 +0300

    5

--- index.js
+++ index.js
@@ -1,52 +1,8 @@
-
-const table = document.querySelector(".checkers-field"); 
-let focusEl={chip: 0,
-            orederTo_upR: true,
-            orederTo_upL: true, 
-            orederTo_dnR: true, 
-            orederTo_dnL: true
-        }
-
-
-table.addEventListener("click", (e) =>{
-    
-    if(e.target.classList.contains('chip')) {
-        focusOfChip(e.target)
-    }
-
-
-    if(e.target.classList.contains('move_option') || e.target.classList.contains('move_option_beat')){
-        
-        if(focusEl.distantOfBeatChip>0){
-           beatOfChip(e.target)
-        }
-        
-        //дамка
-        if(table.rows[focusEl.row].cells[focusEl.cell].children[0].classList.contains("chip_color_red") 
-            && e.target.parentNode.rowIndex == 8){
-                table.rows[focusEl.row].cells[focusEl.cell].children[0].classList.add('damka');
-            }
-        else if(table.rows[focusEl.row].cells[focusEl.cell].children[0].classList.contains("chip_color_white") 
-            && e.target.parentNode.rowIndex == 1){
-                table.rows[focusEl.row].cells[focusEl.cell].children[0].classList.add('damka');
-            }
-        
-        e.target.append(table.rows[focusEl.row].cells[focusEl.cell].children[0]);
-        unFocusOfChip();
-
-    }else{
-        // if(focusEl.chip!=0)
-        // unFocusOfChip(); 
-        // console.log(e.target);
-    }
-})
-
-// e.target.parentNode.rowIndex, e.target.cellIndex
-
 function focusOfChip(chip){
 
     if(!chip.classList.contains('chip_target')) {
-        if (focusEl.chip != 0){
+       
+        if (focusEl.chip !== 0){
             unFocusOfChip();
         }
 
@@ -69,6 +25,7 @@ function focusOfChip(chip){
     }else{
         removeOptionMove('all')
         chip.classList.remove('chip_target')    
+        unFocusOfChip()
     } 
         
 }
@@ -132,22 +89,26 @@ function addClassOfOptionMove(deltRow, deltCell){
 }
 
 function addOptionForDamka(deltRow, deltCell, orederTo){
-    
+    let i=1;
     if(table.rows[focusEl.row +deltRow ].cells[focusEl.cell +deltCell] && orederTo){
         if(table.rows[focusEl.row +deltRow ].cells[focusEl.cell +deltCell].children[0] !== undefined){
         // проверка на пустую ячейку
             if(!table.rows[focusEl.row +deltRow ].cells[focusEl.cell +deltCell ].children[0].classList.contains(`chip_color_${focusEl.color}`)){ 
             //проверка шашки на битость
-                if(table.rows[focusEl.row +Math.sign(deltRow)*(Math.abs(deltRow)+1)].cells[focusEl.cell +Math.sign(deltCell)*(Math.abs(deltCell)+1)] !== undefined && 
-                    !table.rows[focusEl.row + Math.sign(deltRow)*(Math.abs(deltRow)+1)].classList.contains("word")){
+                while(table.rows[focusEl.row +Math.sign(deltRow)*(Math.abs(deltRow)+i)].cells[focusEl.cell +Math.sign(deltCell)*(Math.abs(deltCell)+i)] !== undefined && 
+                !table.rows[focusEl.row + Math.sign(deltRow)*(Math.abs(deltRow)+i)].classList.contains("word")){
+                // if(table.rows[focusEl.row +Math.sign(deltRow)*(Math.abs(deltRow)+1)].cells[focusEl.cell +Math.sign(deltCell)*(Math.abs(deltCell)+1)] !== undefined && 
+                    // !table.rows[focusEl.row + Math.sign(deltRow)*(Math.abs(deltRow)+1)].classList.contains("word")){
+
                 //проверка на границу поля и на пустату x2 клетки 
-                    if(!table.rows[focusEl.row + Math.sign(deltRow)*(Math.abs(deltRow) +1)].cells[focusEl.cell +Math.sign(deltCell)*(Math.abs(deltCell)+1)].childNodes.length){
+                    if(!table.rows[focusEl.row + Math.sign(deltRow)*(Math.abs(deltRow) +i)].cells[focusEl.cell +Math.sign(deltCell)*(Math.abs(deltCell)+i)].childNodes.length){
                         //проверка на не свою шашку за битой
-                        table.rows[focusEl.row +Math.sign(deltRow)*(Math.abs(deltRow)+1 )]
-                        .cells[focusEl.cell +Math.sign(deltCell)*(Math.abs(deltCell)+1)].classList.add("move_option_beat");
+                        table.rows[focusEl.row +Math.sign(deltRow)*(Math.abs(deltRow)+i )]
+                        .cells[focusEl.cell +Math.sign(deltCell)*(Math.abs(deltCell)+i)].classList.add("move_option_beat");
 // Добовлять пока не будет шашка или край
                         focusEl.distantOfBeatChip = Math.abs(deltCell);
                     }
+                    i++;
                 }
             }else  return false;
         }else {
@@ -190,25 +151,71 @@ function beatOfChip(cellClick){
     table.rows[focusEl.row + (focusEl.distantOfBeatChip* Math.sign(cellClick.parentNode.rowIndex - focusEl.row))]
         .cells[focusEl.cell + (focusEl.distantOfBeatChip * Math.sign(cellClick.cellIndex - focusEl.cell))].innerHTML = '' ;
 
-        table.rows[focusEl.row + (focusEl.distantOfBeatChip* Math.sign(cellClick.parentNode.rowIndex - focusEl.row))]
-        .cells[focusEl.cell + (focusEl.distantOfBeatChip * Math.sign(cellClick.cellIndex - focusEl.cell))].style.background = 'green' ;
-
     focusEl.distantOfBeatChip=0;
+    unFocusOfChip()
 }
 
 function start(){
-    // for (let x = 1; x <= table.rows.length-2; x++){
-    //  add ID!!!
-    //     if(x%2==0){
-    //         for (let y = 0; y <= 7; y = y+2){
-    //             table.rows[x].cells[y].innerHTML = '<div class= "chip"></div>';
-    //             console.log();
-    //         }
-    //     }else 
-    //         for (let y = 1; y <= 7; y = y+2){
-    //             table.rows[x].cells[y].innerHTML = '<div class= "chip"></div>';
-    //             console.log();
-    //         }
+    for (let x = 1; x <= 8; x++){
+        if(x%2==0){
+            for (let y = 0; y <= 7; y = y+2){
+                if (x<=3) table.rows[x].cells[y].innerHTML = `<div class= "chip chip_color_red" id="r${x}x${y}"></div>`;
+                if(x>5) table.rows[x].cells[y].innerHTML = `<div class= "chip chip_color_white" id="w${x}x${y}"></div>` ;
+            }
+        }else if(x%2==1){
+            for (let y = 1; y <= 7; y = y+2){
+                if(x<=3) table.rows[x].cells[y].innerHTML = `<div class= "chip chip_color_red" id="r${x}x${y}"></div>`;
+                if(x>5) table.rows[x].cells[y].innerHTML = `<div class= "chip chip_color_white" id="w${x}x${y}"></div>`;
+            }
+        }
     
-    // }
+    }
 }
+
+
+const table = document.querySelector(".checkers-field"); 
+let focusEl={chip: 0,
+            orederTo_upR: true,
+            orederTo_upL: true, 
+            orederTo_dnR: true, 
+            orederTo_dnL: true
+            }
+
+    start()
+
+table.addEventListener("click", (e) =>{
+    
+    if(e.target.classList.contains('chip')) {
+        focusOfChip(e.target)
+        console.log(focusEl.chip);
+    }
+
+
+    if(e.target.classList.contains('move_option') || e.target.classList.contains('move_option_beat')){
+        
+        if(focusEl.distantOfBeatChip>0){
+           beatOfChip(e.target)
+        }
+        
+        //дамка
+        if(table.rows[focusEl.row].cells[focusEl.cell].children[0].classList.contains("chip_color_red") 
+            && e.target.parentNode.rowIndex == 8){
+                table.rows[focusEl.row].cells[focusEl.cell].children[0].classList.add('damka');
+            }
+        else if(table.rows[focusEl.row].cells[focusEl.cell].children[0].classList.contains("chip_color_white") 
+            && e.target.parentNode.rowIndex == 1){
+                table.rows[focusEl.row].cells[focusEl.cell].children[0].classList.add('damka');
+            }
+        
+        e.target.append(table.rows[focusEl.row].cells[focusEl.cell].children[0]);
+        unFocusOfChip();
+
+    }else{
+        // if(focusEl.chip!=0)
+        // unFocusOfChip(); 
+        // console.log(e.target);
+    }
+})
+
+
+// e.target.parentNode.rowIndex, e.target.cellIndex
